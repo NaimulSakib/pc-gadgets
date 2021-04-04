04b8fc450dfad68b7ed3c0f61406ac58a349e597
NaimulSakib <naimulsakib.88@gmail.com>
2021-04-05 01:04:23 +0600

    16- Built cart screens {13}

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
         <header className="row">
           <div>
             <Link className="brand" to="/">
-              amazona
+              PcGadgets
             </Link>
           </div>
           <div>
