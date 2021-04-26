fa8ff87a72a2f71dbe8a7cec67fa0bb6247e2392
NaimulSakib <naimulsakib.88@gmail.com>
2021-04-26 16:10:08 +0600

    27-Create-Place-Order-API {24}

--- backend/utils.js
+++ backend/utils.js
@@ -1,5 +1,4 @@
 import jwt from 'jsonwebtoken';
-
 export const generateToken = (user) => {
   return jwt.sign(
     {
@@ -13,4 +12,25 @@ export const generateToken = (user) => {
       expiresIn: '30d',
     }
   );
+};
+
+export const isAuth = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (authorization) {
+    const token = authorization.slice(7, authorization.length); // Bearer XXXXXX
+    jwt.verify(
+      token,
+      process.env.JWT_SECRET || 'somethingsecret',
+      (err, decode) => {
+        if (err) {
+          res.status(401).send({ message: 'Invalid Token' });
+        } else {
+          req.user = decode;
+          next();
+        }
+      }
+    );
+  } else {
+    res.status(401).send({ message: 'No Token' });
+  }
 };
