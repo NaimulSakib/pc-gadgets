9815511746e4393c3fd8d1c98b6add6990fa1efa
NaimulSakib <naimulsakib.88@gmail.com>
2021-04-28 16:09:24 +0600

    33-Display-User-Profile {31}

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -54,5 +54,15 @@ userRouter.post(
     });
   })
 );
-
+userRouter.get(
+  '/:id',
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      res.send(user);
+    } else {
+      res.status(404).send({ message: 'User Not Found' });
+    }
+  })
+);
 export default userRouter;
