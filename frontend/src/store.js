f82555e99db24dc589c8d43772b4d0307308ce43
NaimulSakib <naimulsakib.88@gmail.com>
2021-04-28 17:40:09 +0600

    34-Update-User-Profile {32}

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -15,6 +15,7 @@ import {
   userDetailsReducer,
   userRegisterReducer,
   userSigninReducer,
+  userUpdateProfileReducer,
 } from './reducers/userReducers';
 const initialState = {
   userSignin: {
@@ -43,6 +44,7 @@ const reducer = combineReducers({
   orderPay: orderPayReducer,
   orderMineList: orderMineListReducer,
   userDetails: userDetailsReducer,
+  userUpdateProfile: userUpdateProfileReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
