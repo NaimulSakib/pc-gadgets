7520de2ffaebf6153280bfb8150384c2c68c68d1
NaimulSakib <naimulsakib.88@gmail.com>
2021-04-30 11:20:47 +0600

    43-Delete-Order {41}

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,6 +3,7 @@ import thunk from 'redux-thunk';
 import { cartReducer } from './reducers/cartReducers';
 import {
   orderCreateReducer,
+  orderDeleteReducer,
   orderDetailsReducer,
   orderListReducer,
   orderMineListReducer,
@@ -53,6 +54,7 @@ const reducer = combineReducers({
   productUpdate: productUpdateReducer,
   productDelete: productDeleteReducer,
   orderList: orderListReducer,
+  orderDelete: orderDeleteReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
