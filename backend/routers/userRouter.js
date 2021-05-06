d56a9053e7d7181d82a7f4a1d4fee7e04d6b178f
NaimulSakib <naimulsakib.88@gmail.com>
2021-05-06 11:20:59 +0600

    #51-corousal added & 53-search box and search option

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -6,7 +6,15 @@ import User from '../models/userModel.js';
 import { generateToken, isAdmin, isAuth } from '../utils.js';
 
 const userRouter = express.Router();
-
+userRouter.get(
+  '/top-sellers',
+  expressAsyncHandler(async (req, res) => {
+    const topSellers = await User.find({ isSeller: true })
+      .sort({ 'seller.rating': -1 })
+      .limit(3);
+    res.send(topSellers);
+  })
+);
 userRouter.get(
   '/seed',
   expressAsyncHandler(async (req, res) => {
--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -22,6 +22,9 @@ import {
   USER_UPDATE_PROFILE_REQUEST,
   USER_UPDATE_PROFILE_RESET,
   USER_UPDATE_PROFILE_SUCCESS,
+  USER_TOPSELLERS_LIST_FAIL,
+  USER_TOPSELLERS_LIST_REQUEST,
+  USER_TOPSELLERS_LIST_SUCCESS,
   USER_UPDATE_REQUEST,
   USER_UPDATE_RESET,
   USER_UPDATE_SUCCESS,
@@ -117,6 +120,18 @@ export const userDeleteReducer = (state = {}, action) => {
       return { loading: false, error: action.payload };
     case USER_DELETE_RESET:
       return {};
+    default:
+      return state;
+  }
+};
+export const userTopSellerListReducer = (state = { loading: true }, action) => {
+  switch (action.type) {
+    case USER_TOPSELLERS_LIST_REQUEST:
+      return { loading: true };
+    case USER_TOPSELLERS_LIST_SUCCESS:
+      return { loading: false, users: action.payload };
+    case USER_TOPSELLERS_LIST_FAIL:
+      return { loading: false, error: action.payload };
     default:
       return state;
   }
